295060f87390382355df0cae0e86d9987515dd38
Chitoku YATO <cyato@nvidia.com>
2025-06-04 20:15:01 +0000

    Skip deprecated bandwidthTest sample

--- packages/cuda/cuda/test-samples.sh
+++ packages/cuda/cuda/test-samples.sh
@@ -5,7 +5,25 @@ set -ex
 
 cd $CUDA_SAMPLES_ROOT/bin/$(uname -m)/linux/release
 
+# Get CUDA version
+if command -v nvcc &> /dev/null; then
+    CUDA_VERSION=$(nvcc --version | grep -o ".*release [0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*")
+else
+    CUDA_VERSION=""
+fi
+
 ./deviceQuery
-./bandwidthTest
+
+# Only run bandwidthTest if CUDA version is less than 12.9
+if [[ -z "$CUDA_VERSION" ]] || awk 'BEGIN{exit ARGV[1]>=12.9?1:0}' "$CUDA_VERSION"; then
+    ./bandwidthTest
+else
+    echo "Skipping ./bandwidthTest for CUDA version >= 12.9 ($CUDA_VERSION)"
+fi
+
+# NVBandwidth utility is to replace the CUDA Sample's bandwidthTest.
+# https://github.com/nvidia/nvbandwidth
+/opt/nvbandwidth/nvbandwidth -j
+
 ./vectorAdd
 ./matrixMul
